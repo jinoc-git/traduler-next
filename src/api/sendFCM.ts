4bdca5dd6d5a903e7fc1a31fd62ee5fc68d1cc7d
jinoc <choiae369@gmail.com>
2024-07-02 00:32:16 +0900

    Fix: firebase admin private key 오류 수정 시도

--- src/api/sendFCM.ts
+++ src/api/sendFCM.ts
@@ -14,7 +14,8 @@ export interface NotificationData {
 export const sendFCMNotification = async (data: NotificationData) => {
   const serviceAccount: ServiceAccount = {
     projectId: process.env.NEXT_PUBLIC_FB_PROJECT_ID,
-    privateKey: process.env.NEXT_PUBLIC_FB_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+    privateKey: process.env.NEXT_PUBLIC_FB_PRIVATE_KEY,
+    // ?.replace(/\\n/g, '\n')
     clientEmail: process.env.NEXT_PUBLIC_CLIENT_EMAIL,
   };
 
