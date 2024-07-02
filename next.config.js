ee791065adbec44bdad8ab61ddd0991c7477e1c2
jinoc <choiae369@gmail.com>
2024-07-02 18:35:49 +0900

    Fix: 기존 ducanh2912 대신 next-pwa 패키지 사용
    - next.config에 관련 설정 변경

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 const withPWA = require('next-pwa')({
   dest: 'public',
-  customWorkerSrc: '/firebase-messaging-sw.js',
-  cacheOnFrontEndNav: true,
-  aggressiveFrontEndNavCaching: true,
+  sw: '/firebase-messaging-sw.js',
+  register: true,
   skipWaiting: true,
   reloadOnOnline: true,
   disable: process.env.NODE_ENV === 'development',
