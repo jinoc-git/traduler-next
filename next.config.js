75b96a3853491ee7972a9fafa92e0701d82dc826
jinoc <choiae369@gmail.com>
2025-05-01 01:35:57 +0900

    fix: next.config.js 파일 vercel build 오류 수정

--- next.config.js
+++ next.config.js
@@ -1,66 +1,67 @@
-const withSerwistInit = require('@serwist/next').default;
+const { PHASE_DEVELOPMENT_SERVER, PHASE_PRODUCTION_BUILD } = require('next/constants');
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: false,
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'rkdykaeilrlrtrowawoe.supabase.co',
-      },
-      {
-        protocol: 'http',
-        hostname: 'k.kakaocdn.net',
-      },
-      {
-        protocol: 'http',
-        hostname: 't1.kakaocdn.net',
-      },
-      {
-        protocol: 'https',
-        hostname: 'lh3.googleusercontent.com',
-      },
-    ],
-  },
-  experimental: {
-    serverActions: true,
-    scrollRestoration: true,
-  },
-  webpack(config) {
-    const fileLoaderRule = config.module.rules.find((rule) => rule.test?.test?.('.svg'));
+/** @type {(phase: string, defaultConfig: import("next").NextConfig) => Promise<import("next").NextConfig>} */
+module.exports = async (phase) => {
+  /** @type {import("next").NextConfig} */
+  const nextConfig = {
+    reactStrictMode: false,
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'rkdykaeilrlrtrowawoe.supabase.co',
+        },
+        {
+          protocol: 'http',
+          hostname: 'k.kakaocdn.net',
+        },
+        {
+          protocol: 'http',
+          hostname: 't1.kakaocdn.net',
+        },
+        {
+          protocol: 'https',
+          hostname: 'lh3.googleusercontent.com',
+        },
+      ],
+    },
+    experimental: {
+      serverActions: true,
+      scrollRestoration: true,
+    },
+    webpack(config) {
+      const fileLoaderRule = config.module.rules.find((rule) => rule.test?.test?.('.svg'));
 
-    config.module.rules.push(
-      {
-        ...fileLoaderRule,
-        test: /\.svg$/i,
-        resourceQuery: /url/,
-      },
-      {
-        test: /\.svg$/i,
-        issuer: fileLoaderRule.issuer,
-        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] },
-        use: ['@svgr/webpack'],
-      },
-    );
+      config.module.rules.push(
+        {
+          ...fileLoaderRule,
+          test: /\.svg$/i,
+          resourceQuery: /url/,
+        },
+        {
+          test: /\.svg$/i,
+          issuer: fileLoaderRule.issuer,
+          resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] },
+          use: ['@svgr/webpack'],
+        },
+      );
 
-    fileLoaderRule.exclude = /\.svg$/i;
+      fileLoaderRule.exclude = /\.svg$/i;
 
-    return config;
-  },
+      return config;
+    },
+  };
+
+  if (phase === PHASE_DEVELOPMENT_SERVER || phase === PHASE_PRODUCTION_BUILD) {
+    const withSerwist = (await import('@serwist/next')).default({
+      swSrc: 'app/sw.ts',
+      swDest: 'public/sw.js',
+      disable: process.env.NODE_ENV === 'development',
+      // cacheOnNavigation: true,
+      // additionalPrecacheEntries: [{ url: '/~offline', revision }],
+    });
+    return withSerwist(nextConfig);
+  }
+
+  return nextConfig;
 };
-
-const isProd = process.env.NODE_ENV === 'production';
-const noWrapper = (config) => config;
-// const revision = crypto.randomUUID();
-
-const serwistConfig = {
-  swSrc: 'app/sw.ts',
-  swDest: 'public/sw.js',
-  // cacheOnNavigation: true,
-  // additionalPrecacheEntries: [{ url: '/~offline', revision }],
-};
-
-const withPWA = isProd ? withSerwistInit(serwistConfig) : noWrapper;
-
-module.exports = withPWA(nextConfig);
